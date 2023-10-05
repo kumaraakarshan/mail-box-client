7304f49f33c2e11b76922de210f621220741871a
kumaraakarshan <aakarshan0005@gmail.com>
2023-10-06 05:00:29 +0530

    login signup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,7 @@
-import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import { LoginContextProvider } from "./Components/Context/LoginContext";
-import Welcome from "./Components/Pages/Welcome";
-
-import "./App.css";
-
-import SignUp from "./Components/Pages/SignUp";
-import SignIn from "./Components/Pages/SignIn";
+import AuthForm from "./Components/AuthForm";
 
 function App() {
-  return (
-    <React.Fragment>
-      <LoginContextProvider>
-        {/* <Route path="*">
-          <Redirect to="/signUp" />
-        </Route> */}
-
-        <Route path="/signUp">
-          <SignUp />
-        </Route>
-
-        <Route path="/signIn">
-          <SignIn />
-        </Route>
-
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-      </LoginContextProvider>
-    </React.Fragment>
-  );
+  return <AuthForm />;
 }
 
 export default App;
